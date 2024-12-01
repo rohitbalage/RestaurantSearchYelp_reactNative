c958aa7d4112b0b98dd60366c5ac936f9feea126
Rohit Balage <thatcomputerboy26@gmail.com>
2024-12-01 11:26:33 -0500

    fixed problem of react-navigation-stack

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,18 @@
+import React  from "react";
+import {View, Text, StyleSheet} from 'react-native';
+
+
+const SearchScreen = () =>
+{
+
+return <View>
+
+<Text>
+Search Screeen
+</Text>
+</View>
+
+};
+
+const styles = StyleSheet.create({});
+export default SearchScreen;
